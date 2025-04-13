2c3cbea98eed56915a0ccb180b36134744259b91
Bea Sanssi <bs222eh@student.lnu.se>
2025-04-13 17:37:15 +0200

    Start building up backend to be structured according to SOLID

---
+++ backend/src/services/ProductService.js
@@ -0,0 +1,34 @@
+/**
+ * @file This file contains the ProductService class
+ * @module ProductService
+ * @author Beatriz Sanssi
+*/
+
+/**
+ * The ProductService class provides methods to interact with the product repository.
+ * It allows searching for products by name and retrieving all products.
+ */
+export class ProductService {
+  constructor(productRepository) {
+    this.productRepository = productRepository
+  }
+
+  /**
+   * Searches for products by name.
+   *
+   * @param {string} query - The search query.
+   * @returns {Promise<Array>} - A promise that resolves to an array of products.
+   */
+  async search(query) {
+    return this.productRepository.get({ product_name: new RegExp(query, 'i') })
+  }
+
+  /**
+   * Retrieves all products.
+   *
+   * @returns {Promise<Array>} - A promise that resolves to an array of all products.
+   */
+  async findAll() {
+    return this.productRepository.get({})
+  }
+}
