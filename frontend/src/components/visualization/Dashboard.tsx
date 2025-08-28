320aaf8e2f763ebb0fba17a476eaec52de3d6632
Bea Sanssi <bs222eh@student.lnu.se>
2025-08-28 12:10:49 +0200

    WIP: Fixing layout issue

--- frontend/src/components/visualization/Dashboard.tsx
+++ frontend/src/components/visualization/Dashboard.tsx
@@ -71,7 +71,7 @@ export default function Dashboard({ ecoFilter = [] }: Props) {
   return (
     <ReactECharts
       option={option}
-      style={{ width: '100%', height: '100%' }}
+      style={{ width: '100%', height: '400px' }}
     />
   )
 }
