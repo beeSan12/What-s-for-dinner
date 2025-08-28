9b40da79b15e71f540a843ca25ea4248a760c86b
Bea Sanssi <bs222eh@student.lnu.se>
2025-08-28 11:47:12 +0200

    Update styling

--- frontend/src/components/visualization/Dashboard.tsx
+++ frontend/src/components/visualization/Dashboard.tsx
@@ -71,7 +71,7 @@ export default function Dashboard({ ecoFilter = [] }: Props) {
   return (
     <ReactECharts
       option={option}
-      style={{ maxWidth: '100%', width: '100%', height: '100%' }}
+      style={{ width: '100%', height: '100%' }}
     />
   )
 }
